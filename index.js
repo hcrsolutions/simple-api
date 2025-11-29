ed81f00648da1aace20b7d5ea9ec27f5de3db333
hcrsolutions <hcrsolutions.12.gmail.com>
2025-11-29 13:44:23 +0530

    Update message for CI/CD test

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Simple API v1 — CI/CD working!");
+  res.send("Simple API v2 — CI/CD is working!");
 });
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-// test deploy
