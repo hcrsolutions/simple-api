000d22aa33045878483064c0deb0eb6ab4d4d093
hcrsolutions <hcrsolutions.12.gmail.com>
2025-11-29 13:21:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Simple API v1 — CI/CD working!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
