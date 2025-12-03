25e4b6216050866f821582d725933e701816fb8d
hcrsolutions <hcrsolutions.12.gmail.com>
2025-12-03 11:21:25 +0530

    Add new feature route /demo

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
 app.get("/new", (req, res) => {
   res.send("This is a new feature route!");
 });
+
+app.get("/demo", (req, res) => {
+  res.send("This is a demo feature route!");
+});
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
